236874470a9e2f0a37a47eb7eb73e0e107e57392
Asim Jamal <jamalasim081@gmail.com>
2018-09-18 16:37:52 +0530

    Assignment 2

---
+++ Assignment2.py
@@ -0,0 +1,18 @@
+print("anything you want")
+
+print("Acad" + "View")
+
+x = input('Input X:')
+y = input('Input Y:')
+z = input('Input Z:')
+
+print(x,y,z)
+print("Lets get Started")
+s = 'Acadview'
+course = 'Python'
+fees = 5000
+print ('%s %s %d' % (s, course, fees))
+r = float(input('Enter radius:'))
+pi = 3.14
+area = pi*r*r
+print ('%f is the area of circle' %area)
