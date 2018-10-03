9b98f94769a9b46914c4122b8541e1bf552ce693
Asim Jamal <jamalasim081@gmail.com>
2018-10-03 15:42:13 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print("Printing all over")
+list1 = [1, 2, 3, 4]
+list2 = [4, 5, 6, 7, 8, 4, 4, 4,]
+list3 = list1 + list2
+print(list2.count('4'))
+list2.sort()
+print("Sorted list2 : ", list2)
+mytuple = ('asim', 'jamal', 'robert', 'thor')
+rev_mytuple = mytuple[::-1] 
+print(rev_mytuple)
+print(max(rev_mytuple))
+print(min(rev_mytuple))
+s = "sublime text rockxx"
+print(s.upper())
+strng = 'Hello World'
+print(strng)
+new_strng = strng.replace('World', 'Asim')
+print(new_strng)
