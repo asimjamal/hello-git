0e761529af8a88a512609b53b33f69fb91fd329f
Asim Jamal <jamalasim081@gmail.com>
2018-10-09 11:57:40 +0530

    Add files via upload

---
+++ Assignment6.py
@@ -0,0 +1,33 @@
+# def sphr():
+#  r= int(input("Enter The radius:"))
+#  area = 4*3.14*r*r
+#  print(area)
+
+# sphr()
+
+# def perfect(i):
+# 	list1=[]
+
+# 	for j in range(1,1000):
+# 		if (i%j == 0 and i!=j):
+# 		  list1.append(j)
+		   
+# 	b= sum(list1)
+# 	if i==b:
+# 		print('This is a perfect number',i)
+
+
+# for i in range(1,1000):
+# 	perfect(i)
+
+# n = int(input("Enter Number:"))
+# for i in range(1,11):
+# 	print(n*i)
+
+# def power(i,j):
+# 	for x in range(1,j+1):
+# 	 po = i**x
+# 	print(po)
+
+# power(2,3)
+
